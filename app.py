72c5fba76e59082c8561b8af00ea83d438ecaf27
Ratko <sopicratko@gmail.com>
2023-06-02 00:17:54 +0200

    Dodavanje gresaka za kasniju popravku
    Hešovanje lozinke i kasnije verifikovanje

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-#Importovanje Flaska i MYSQL-a--------------------
-from flask import Flask, session, render_template, url_for, request
+#Importovanje Flaska, MYSQL-a i heša--------------------
+from flask import Flask, session, render_template, url_for, request, flash
 from flask_mysqldb import MySQL
+import yaml
+from passlib.hash import sha256_crypt
 #---------------------------------------
 
 #Importovanje Bootstrap paketa
@@ -18,6 +20,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PythonZadatak'
 bootstrap = Bootstrap(app)
 #
+
+#KONFIG MYSQLA
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+#--------------------------
+
 
 class RegisterForm(FlaskForm):
     FirstNme = StringField('Unesite Vase ime', validators=[DataRequired()])
@@ -42,6 +54,17 @@ def register():
         FirstName = request.form.get('name')
         LastName = request.form.get('surname')
         email = request.form.get('email')
-
-
-
+        username = request.form.get('username')
+        pswrd = sha256_crypt.hash(request.form.get('pswrd'))
+        verify_pass = request.form.get('verifypswrd')
+        #Ako je ispravna sifra unesena oba puta komituje se u bazu
+        if sha256_crypt.verify(request.form.get('verifypswrd'), pswrd) == True:
+            cur.execute('INSERT INTO users(firstname, lastname, username, email, password) VALUES (%s, %s, %s, %s, %s)', [FirstName, LastName, username, email, pswrd])
+            mysql.connection.commit()
+            cur.close()
+        else:
+            flash('Lozinke se ne podudaraju, pokušajte ponovo.')
+            return render_template('register.html', form=form)
+            
+            
+    return render_template('register.html', form=form)
