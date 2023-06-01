fe151f64faf29c0c37b996df06d15c02a5f7d7ee
Ratko <sopicratko@gmail.com>
2023-06-01 19:02:59 +0200

    Postavka fajlova

---
+++ venv/app.py
@@ -0,0 +1,44 @@
+#Importovanje Flaska i MYSQL-a--------------------
+from flask import Flask, session, render_template, url_for, request
+from flask_mysqldb import MySQL
+#---------------------------------------
+
+#Importovanje Bootstrap paketa
+from flask_bootstrap import Bootstrap
+#---------------------------------
+
+#Importovanje WTF Formi--------------------
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+#-------------------------------------------
+
+#Konfigurisanje Flaska i Bootstrapa
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'PythonZadatak'
+bootstrap = Bootstrap(app)
+#
+
+class RegisterForm(FlaskForm):
+    FirstNme = StringField('Unesite Vase ime', validators=[DataRequired()])
+    LastName = StringField('Unesite Vase prezime', validators=[DataRequired()])
+    email = StringField('Unesite Vasu email adresu', validators=[DataRequired()])
+    username = StringField('Unesite željeno korisničko ime', validators=[DataRequired()])
+    pswrd = PasswordField('Unesite željenu lozinku', validators=[DataRequired()])
+    verifypswrd = PasswordField('Ponovo unesite lozinku', validators=[DataRequired()])
+    submit = SubmitField('Registracija', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Unesite korisničko ime', validators=[DataRequired()])
+    pswrd = PasswordField('Unesite lozinku', validators=[DataRequired()])
+    submit = SubmitField('Log In', validators=[DataRequired()])
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST':
+        cur = MySQL.connection.cursor()
+
+
+
